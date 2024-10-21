691fa3c8cf0d7b71095ebff62e95b59892115c84
abhisheks@prophecy.io <abhisheks@prophecy.io>
2024-10-21 06:26:37 +0000

    Update schema and SQL snapshot for write_modes_others to change data types and add timestamp column

--- snapshots/write_modes_others/uitesting_scd2_diff_col_val.sql
+++ snapshots/write_modes_others/uitesting_scd2_diff_col_val.sql
@@ -5,7 +5,7 @@
     "strategy": 'timestamp',
     "target_schema":  var('qa_schema') ,
     "unique_key": 'c_id',
-    "updated_at": "c_float"
+    "updated_at": "c_timestamp"
   })
 }}
 
@@ -31,7 +31,8 @@ all_type_non_partitioned_columns AS (
     c_boolean AS c_boolean,
     c_array AS c_array,
     c_struct AS c_struct,
-    monotonically_increasing_id() AS c_id
+    monotonically_increasing_id() AS c_id,
+    CURRENT_TIMESTAMP() AS c_timestamp
   
   FROM all_type_non_partitioned_1 AS in0
 
